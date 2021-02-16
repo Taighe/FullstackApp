e45f47f71fae83cc8ed84a3f00b3c1fd372c61c4
Taighe <taighe.wh@gmail.com>
2021-02-16 17:57:04 +1100

    +Added components ~supported nav links and angular routing +Added toast service for notifications +Added route guards

--- ASP_MVCNetCoreExample/client/src/app/app-routing.module.ts
+++ ASP_MVCNetCoreExample/client/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MovieDetailComponent } from './movies/movie-detail/movie-detail.component';
+import { MovieListComponent } from './movies/movie-list/movie-list.component';
+import { AuthGuard } from './_guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'lists', component: ListsComponent },
+      { path: 'movies', component: MovieListComponent, canActivate: [AuthGuard] },
+      { path: 'movies/:id', component: MovieDetailComponent }
+    ]
+  },
+
+  { path: '**', component: HomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ASP_MVCNetCoreExample/client/src/app/nav/nav.component.ts
+++ ASP_MVCNetCoreExample/client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { User } from '../_models/user';
@@ -13,21 +15,25 @@ export class NavComponent implements OnInit {
   model: any = {}
   currentUser: User
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {
+
+  }
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/movies');
       },
       error => {
         console.log(error);
+        this.toastr.error(error.error);
       })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
